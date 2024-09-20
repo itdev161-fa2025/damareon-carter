6a13554381dc30f62c697904582721ae9987a863
DamareonCarter <carted92@gmatc.matc.edu>
2024-09-20 06:39:24 -0500

    Completed Activity 3

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const db = require('./config/db');
+const check = require('express-validator').check;
+const validationResult = require('express-validator').validationResult;
 
 let app = express();
 
@@ -8,11 +10,20 @@ db.connectDatabase();
 app.use(express.json({ extended: false }));
 
 app.get('/', (req, res) => res.send('HTTP GET request sent to root API endpoint'));
-app.post('/api/users', (req, res) => 
-{
-    console.log(req.body);
-    res.send(req.body);
-});
+app.post('/api/users',
+    [
+        check('name', 'Please enter your name').not().isEmpty(),
+        check('email', 'Please enter a valid email').isEmail(),
+        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+    ],
+    (req, res) => 
+    {
+        const errors = validationResult(req);
+
+        if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
+        else res.send(req.body);
+    }
+);
 
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
