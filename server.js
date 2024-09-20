17d5ddafa0f2ebab39b0d6a0915b39f160066e0f
DamareonCarter <carted92@gmatc.matc.edu>
2024-09-20 06:10:11 -0500

    Created Registration Endpoint

--- server.js
+++ server.js
@@ -5,6 +5,14 @@ let app = express();
 
 db.connectDatabase();
 
+app.use(express.json({ extended: false }));
+
 app.get('/', (req, res) => res.send('HTTP GET request sent to root API endpoint'));
+app.post('/api/users', (req, res) => 
+{
+    console.log(req.body);
+    res.send(req.body);
+});
+
 
 app.listen(3000, () => console.log('Express server running on port 3000'));
