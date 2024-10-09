41b9cccff9020c6f6bcede211ec26d2cf19125cd
DamareonCarter <carted92@gmatc.matc.edu>
2024-10-09 01:38:00 -0500

    Changed server.js

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const db = require('./config/db');
 const check = require('express-validator').check;
 const validationResult = require('express-validator').validationResult;
 const cors = require('cors');
+const User = require('./models/User').User;
+const bcrypt = require('bcryptjs');
 
 let app = express();
 
@@ -18,12 +20,36 @@ app.post('/api/users',
         check('email', 'Please enter a valid email').isEmail(),
         check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
     ],
-    (req, res) => 
+    async (req, res) => 
     {
         const errors = validationResult(req);
 
         if (!errors.isEmpty()) return res.status(422).json({ errors: errors.array() });
-        else res.send(req.body);
+        else
+        {
+            const { name, email, password } = req.body;
+
+            try
+            {
+                let user = await User.findOne({ email: email});
+
+                if (user)
+                {
+                    return res.status(400).json({ errors: [{ msg: "User already exists" }] });
+                }
+
+                user = new User({ name: name, email: email, password: password });
+                const salt = await bcrypt.genSalt(10);
+                user.password = await bcrypt.hash(password, salt);
+
+                await user.save();
+                res.send("User successfully registered");
+
+            } catch (error)
+            {
+                res.status(500).send("Server error");
+            }
+        }
     }
 );
 
