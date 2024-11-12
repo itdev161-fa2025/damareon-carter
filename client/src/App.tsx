11d5ef578e0fe350dcbc0df4c201513c4ba85bea
DamareonCarter <carted92@gmatc.matc.edu>
2024-11-12 15:45:58 -0600

    Completed part 3

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -10,7 +10,9 @@ class App extends React.Component
 {
     state =
     {
-        data: null
+        data: null,
+        token: null,
+        user: null
     }
 
     componentDidMount()
@@ -18,10 +20,50 @@ class App extends React.Component
         axios.get("http://localhost:5000")
         .then(response => this.setState({data: response.data}))
         .catch(error => console.error(`Error fetching data: ${error}`));
+        this.authenticateUser();
+    }
+
+    authenticateUser = () => {
+        const token = localStorage.getItem('token');
+
+        if (!token) {
+            localStorage.removeItem('user');
+            this.setState({ user: null });
+        }
+
+        if (token) {
+            const config = {
+                headers: {
+                    'x-auth-token': token
+                }
+            };
+
+            axios.get('http://localhost:5000/api/auth', config)
+                .then((response) => {
+                    localStorage.setItem('user', response.data.name);
+                    this.setState({ user: response.data.name })
+                })
+                .catch((error) => {
+                    localStorage.removeItem('user');
+                    this.setState({ user:null });
+                    console.error(`Error logging in: ${error}`);
+                });
+        }
+    }
+
+    logout = () => {
+        localStorage.removeItem('token');
+        localStorage.removeItem('user');
+        this.setState({ user: null, token: null });
     }
 
     render(): React.ReactNode
     {
+        let { user, data } = this.state;
+        const authProps = {
+            authenticateUser: this.authenticateUser
+        };
+
         return (
             <div className='App'>
                 <header className='App-header'>
@@ -29,12 +71,15 @@ class App extends React.Component
                     <ul>
                         <li><Link to="/">Home</Link></li>
                         <li><Link to="/register">Register</Link></li>
-                        <li><Link to="/login">Login</Link></li>
+                        {user ?
+                            <li><Link to="" onClick={this.logout}>Log out</Link></li> :
+                            <li><Link to="/login">Login</Link></li>
+                        }
                     </ul>
                 </header>
                 <main>
                     <Routes>
-                        <Route path="/" element={<Home/>}/>
+                        <Route path="/" element={<Home user={user} data={data} />}/>
                         <Route path="/register" element={<Register/>}/>
                         <Route path="/login" element={<Login/>}/>
                     </Routes>
