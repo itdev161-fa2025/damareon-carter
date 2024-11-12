7512a820454a32185f8085c4ce79241990786efa
DamareonCarter <carted92@gmatc.matc.edu>
2024-11-12 15:02:12 -0600

    Completed part 1

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ import { User } from './models/Users.js';
 import bcryptjs from 'bcryptjs';
 import jsonwebtoken from 'jsonwebtoken';
 import config  from 'config';
+import auth from "./middleware/auth.js";
 
 let app = express();
 
@@ -15,12 +16,12 @@ connectDatabase();
 app.use(json({ extended: false }));
 app.use(cors({ origin: "http://localhost:3000" }));
 
-app.get('/', (req, res) => res.send('HTTP GET request sent to root API endpoint'));
+app.get('/', (req, res) => res.send("HTTP GET request sent to root API endpoint"));
 app.post('/api/users',
     [
-        check('name', 'Please enter your name').not().isEmpty(),
-        check('email', 'Please enter a valid email').isEmail(),
-        check('password', 'Please enter a password with 6 or more characters').isLength({ min: 6 })
+        check('name', "Please enter your name").not().isEmpty(),
+        check('email', "Please enter a valid email").isEmail(),
+        check('password', "Please enter a password with 6 or more characters").isLength({ min: 6 })
     ],
     async (req, res) => 
     {
@@ -68,6 +69,14 @@ app.post('/api/users',
         }
     }
 );
+app.get('/api/auth', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send("Unkown server error");
+    }
+})
 
 const PORT = 5000;
 app.listen(PORT, () => console.log(`Express server running on port ${PORT}`));
