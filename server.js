0e37a85e95f5f714fae32ae54d8998465e3b8a01
DamareonCarter <carted92@gmatc.matc.edu>
2024-11-12 21:07:07 -0600

    Added post API endpoint

--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 import express, { json } from 'express';
-import { connectDatabase } from './config/db.js';
-import { check } from 'express-validator';
-import { validationResult } from 'express-validator';
+import connectDatabase from './config/db.js';
+import { check, validationResult } from 'express-validator';
 import cors from 'cors';
-import { User } from './models/Users.js';
+import User from './models/Users.js';
 import bcryptjs from 'bcryptjs';
 import jsonwebtoken from 'jsonwebtoken';
 import config  from 'config';
 import auth from "./middleware/auth.js";
+import Post from "./models/Post.js";
 
 let app = express();
 
@@ -17,6 +17,14 @@ app.use(json({ extended: false }));
 app.use(cors({ origin: "http://localhost:3000" }));
 
 app.get('/', (req, res) => res.send("HTTP GET request sent to root API endpoint"));
+app.get('/api/auth', auth, async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id);
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).send("Unkown server error");
+    }
+});
 app.post('/api/users',
     [
         check('name', "Please enter your name").not().isEmpty(),
@@ -55,14 +63,6 @@ app.post('/api/users',
         }
     }
 );
-app.get('/api/auth', auth, async (req, res) => {
-    try {
-        const user = await User.findById(req.user.id);
-        res.status(200).json(user);
-    } catch (error) {
-        res.status(500).send("Unkown server error");
-    }
-});
 app.post('/api/login',
     [
         check('email', "Please enter a valid email").isEmail(),
@@ -95,6 +95,42 @@ app.post('/api/login',
         }
     }
 );
+app.post('/api/posts',
+    [
+        auth,
+        [
+            check('title', "Title text is required").not().isEmpty(),
+            check('body', "Body text is required").not().isEmpty()
+        ]
+    ],
+    async (req, res) => {
+        const errors = validationResult(req);
+
+        if(!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        } else {
+            const { title, body } = req.body;
+
+            try {
+                let user = await User.findById(req.user.id);
+
+                const post = new Post({
+                    user: user.id,
+                    title: title,
+                    body: body
+                });
+
+                await post.save();
+
+                res.json(post);
+            } catch (error) {
+                console.error(error);
+                res.status(500).send("Server error");
+            }
+        }
+    }
+)
+
 const returnToken = (user, res) => {
     const payload = {
         user: {
